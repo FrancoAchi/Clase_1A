da2ff8b4dc0be4a4a5fc7197107c1a8893c1232d
Franco <francoachinelli90@gmail.com>
2023-09-20 17:28:37 -0300

    Tercer commit

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hola mundo")
+import pygame 
+from sys import exit
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+screen.fill((148,0,21))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+
+    pygame.display.flip()
+
+
